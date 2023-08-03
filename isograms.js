e35222441a3c2240cc3a4887fbb23d64328fea34
peninah98 <peninangizwena@gmail.com>
2023-08-03 18:29:40 +0200

    Updates

--- isograms.js
+++ isograms.js
@@ -2,12 +2,7 @@ function isogram(str){
     let set = new Set(str.toLowerCase())
     let result = Array.from(set).join('')
 
-    if(result.length === str.length){
-        return true
-    }
-    else{
-        return false
-    }
+    return result.length == str.length ? true : false
 }
 
-console.log(isogram('hey'))
+console.log(isogram('tree'))
