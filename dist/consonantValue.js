255a67d406af07a440664d39f5a5e9518d083bbd
peninah98 <peninangizwena@gmail.com>
2023-08-28 12:25:11 +0200

    feat : ft/consonantValue

---
+++ dist/consonantValue.js
@@ -0,0 +1,5 @@
+"use strict";
+const solve = (s) => Math.max(...s
+    .split(/[aeiou]/)
+    .map((substr) => substr.split("").reduce((sum, char) => sum + char.charCodeAt(0) - 96, 0)));
+console.log(solve("DEVELOPER"));
