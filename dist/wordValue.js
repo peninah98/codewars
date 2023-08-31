1fbf96d4572fd9ec5072023f13623f2e86fc18e6
peninah98 <peninangizwena@gmail.com>
2023-08-31 11:25:33 +0200

    feat: ft/capitilise 7kyu Alternate capitilize in TypeScript

--- dist/wordValue.js
+++ dist/wordValue.js
@@ -1,12 +1,1 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.wordValue = void 0;
-function wordValue(arr) {
-    return arr.map((mult, index) => mult
-        .replace(/\s/g, "")
-        .split("")
-        .reduce((a, b) => a + b.charCodeAt(0) - 96, 0) *
-        (index + 1));
-}
-exports.wordValue = wordValue;
-console.log(wordValue(["abc", "abc abc"]));
