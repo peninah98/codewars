85a084ac983ca1cf07cf1df2f6e266c0eea52926
peninah98 <peninangizwena@gmail.com>
2023-09-06 12:35:48 +0200

    Removing duplicates

--- dist/removeDuplicates.js
+++ dist/removeDuplicates.js
@@ -1,8 +1,5 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.distinct = void 0;
-const distinct = (a) => {
-    return Array.from(new Set(a));
-};
-exports.distinct = distinct;
-console.log((0, exports.distinct)([1, 2, 3, 2, 3, 23, 2, 3, 22]));
+function removeDuplicateWords(s) {
+    return Array.from(new Set(s.split(" "))).join(' ').toString();
+}
+console.log(removeDuplicateWords("alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta"));
