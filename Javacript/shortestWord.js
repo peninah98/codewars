e9f0c8b553036cb733268ada07f09ae7ec0bbc3e
Penine  Ngizwenayo <peninangizwenayo@gmail.com>
2024-04-30 23:50:26 +0200

    feat: shortest word

--- Javacript/shortestWord.js
+++ Javacript/shortestWord.js
@@ -1,3 +1,9 @@
+// function findShort(s) {
+//   return s.split(" ").sort((a, b) => a.length - b.length)[0].length
+// }
+
 function findShort(s) {
-  return s.split(" ").sort((a, b) => a.length - b.length)[0].length
+  return Math.min(...s.split(" ").map((s) => s.length))
 }
+
+console.log(findShort("ahaganya  agan agfs asjd asdf jkl jss"))
