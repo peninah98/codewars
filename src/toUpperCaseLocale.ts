97136504f12ed3c7f6514a9a9387222801edb8c8
Peninah98 <peninangizwenayo@gmail.com>
2025-02-10 11:21:26 +0200

    feat: create to upper case local function

---
+++ src/toUpperCaseLocale.ts
@@ -0,0 +1,4 @@
+// This function returns the converted string like toUpperCase(): expect for the  local conflict  that conflcits the regular Unicode case mappings such as Turkish
+
+const toUpperCaseLocaleFunc = (string: string)=>string.toLocaleUpperCase()
+console.log(toUpperCaseLocaleFunc("peninah"))
