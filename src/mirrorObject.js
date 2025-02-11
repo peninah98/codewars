b6563ad526ea9b257f8f3bcb2f52a83e002fb07c
Peninah98 <peninangizwenayo@gmail.com>
2025-02-11 11:18:37 +0200

    feat: create a mirrored object

--- src/mirrorObject.js
+++ src/mirrorObject.js
@@ -1,6 +1,41 @@
 const mirrorObject = (string) => {
-  return string.split("").reverse().join("");
+  return string.split("").reverse().join("")
 }
 console.log(mirrorObject("hello"))
 
+const objectMirror = (object) => {
+  return Object.fromEntries(Object.keys(object).map(
+    (key => [key, key.split("").reverse().join("")])
+  ))
+}
 
+console.log(objectMirror({ Penine: "", Noella: "", Momc: "" }))
+
+// const objectMirror = (object) => {
+//     return Object.fromEntries(
+//         Object.keys(object).map(key => [key, key.split("").reverse().join("")])
+//     );
+// };
+
+// console.log(objectMirror({ abc: "", arara: "", xyz: "" }));
+// // Expected Output: { abc: "cba", arara: "arara", xyz: "zyx" }
+
+// console.log(objectMirror({ Penine: "", Noella: "Noella", Momc: "" }));
+// // Output: { Penine: "enineP", Noella: "alleoN", Momc: "cmoM" }
+
+
+// const createObject = (array) => {
+//   return Object.fromEntries(array)
+// }
+
+// console.log(
+//   createObject([
+//     ["Rwanda", "Kigali"],
+//     ["Tanzania", "Dodoma"],
+//     ["Kenya", "Nairobi"],
+//     ["Uganda", "Kampala"],
+//     ["Ethiopia", "Addis Ababa"],
+//     ["Somalia", "Mogadishu"],
+//     ["Burundi", "Bujumbura", "Mbarara", "Akanyaru"],
+//   ])
+// )
