a2a8e99333f34e26420ed3a0d674e8c4c3c6c33b
Peninah98 <peninangizwenayo@gmail.com>
2025-03-01 23:19:30 +0200

    feat: create the fiz buz

--- src/multiplesOf15.js
+++ src/multiplesOf15.js
@@ -1,4 +1,10 @@
 const multipleos = number => {
+    if (typeof number!== 'number' || number <= 0) {
+        throw new Error('Invalid input. Please enter a positive integer.');
+    }
+    if (number % 3 === 0 && number % 5 === 0) {
+        return 'FizzBuzz';
+    }
     return 15 % number === 0;
 }
 
