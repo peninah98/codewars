82a5595c84cc1556fabf23a2229e7380356e1b30
Peninah98 <peninangizwenayo@gmail.com>
2025-03-03 13:48:20 +0200

    feat: create the max number of the array

---
+++ interviewPreparation/maxNumberOfArray.js
@@ -0,0 +1,7 @@
+// Input: Array
+// Returns: number (maximum number of array)
+// Es6 Method: Math.max()
+// function name: maxNumber
+
+const maxNumber = (arr) => Math.max(...arr)
+console.log(maxNumber([1, 43, 5400000, 56, 43, 4, 4444, 4334, 99999]))
