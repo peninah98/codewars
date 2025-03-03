2b87fc6208485fb9bcdd99b7abd11fefaa46ca1d
Peninah98 <peninangizwenayo@gmail.com>
2025-03-03 14:07:57 +0200

    feat: create palimdrome name

--- interviewPreparation/palindrome.js
+++ interviewPreparation/palindrome.js
@@ -1,6 +1,10 @@
-const palindrome = (name) => {
-  if (name === name.split("").reverse().join("")) {
-    return true
-  }
-  return false
-}
+// const palindrome = (name) => {
+//   if (name === name.split("").reverse().join("")) {
+//     return true
+//   }
+//   return false
+// }
+
+const palindromeName = (str)=>str === str.split("").reverse().join("")?true:false
+
+console.log(palindromeName("madam"))
