3b5db723f7d531f7bf5c418ab1fe2ebd7c6f8d33
Peninah98 <peninangizwenayo@gmail.com>
2025-03-05 15:49:42 +0200

    refactor: duplicate function

--- Javacript/duplicate.js
+++ Javacript/duplicate.js
@@ -1,8 +1,8 @@
-const duplicateNumber = (arr) => {
-  for (let i = 0; i < arr.lenght; i++) {
+
+function elimination(arr) {
+  for (let i = 0; i < arr.length; i++) {
     if (arr.indexOf(arr[i]) !== i) return arr[i]
   }
   return null
 }
-
-console.log(duplicateNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3])) 
+console.log(elimination([2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3]))
