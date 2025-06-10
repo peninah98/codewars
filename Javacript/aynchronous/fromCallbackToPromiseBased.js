712d1898defbce02618be47fa936dcdba4c74cfb
Peninah98 <peninangizwenayo@gmail.com>
2025-06-10 09:20:25 +0200

    refactor: refactor callback function into promises based function

--- Javacript/aynchronous/fromCallbackToPromiseBased.js
+++ Javacript/aynchronous/fromCallbackToPromiseBased.js
@@ -1,19 +1,32 @@
+// function fetchData() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       const data = "Data fetched successfully!";
+//       resolve(data);
+//     }, 1000);
+//   });
+// }
+  
+//   fetchData((error, data) => {
+//     if (error) {
+//       console.error("Error:", error);
+//     } else {
+//       console.log(data);
+//     }
+//   });
+
+
 function fetchData() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      const data = "Data fetched successfully!";
-      resolve(data);
+      if (Math.random() > 0.1) {
+        resolve("Data fetched successfully!");
+      } else {
+        reject(new Error("Failed to fetch data"));
+      }
     }, 1000);
   });
 }
-  
-  fetchData((error, data) => {
-    if (error) {
-      console.error("Error:", error);
-    } else {
-      console.log(data);
-    }
-  });
 
 
 
